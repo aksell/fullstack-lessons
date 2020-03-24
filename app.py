f08d52506dc5538500fa316fb124356bf972dd4f
Aksel Lenes <aksel.lenes@gmail.com>
2020-03-24 09:10:22 +0100

    Add a completed column to todo item model
    Migration sets completed to false for all existing records.

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ migrate = Migrate(app, db)
 class TodoItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f"id: {self.id} description: {self.description}"
