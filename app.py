03736a7c9d304788aa734f31c859e71b62adb17e
Aksel Lenes <aksel.lenes@gmail.com>
2020-04-05 17:07:48 +0200

    Make todolist model and database migrations
    Each todo_item is now related to a todo list.

--- app.py
+++ app.py
@@ -12,10 +12,21 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+class TodoList(db.Model):
+    __tablename__ = "todolist"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    todos = db.relationship("TodoItem", backref="list", lazy=True)
+
+    def __repr__(self):
+        return f"TODO List: {self.name}"
+
+
 class TodoItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey("todolist.id"), nullable=False)
 
     def __repr__(self):
         return f"id: {self.id} description: {self.description}"
