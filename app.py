fccc6d677d63dd4ba8a1900289a90300272e69e6
Aksel Lenes <aksel.lenes@gmail.com>
2020-04-05 20:39:22 +0200

    Display list of lists and todos from selected list
    Change the style to display list of lists and list of todos in list
    side by side

--- app.py
+++ app.py
@@ -32,9 +32,18 @@ class TodoItem(db.Model):
         return f"id: {self.id} description: {self.description}"
 
 
+@app.route("/list/<list_id>")
+def get_list_todos(list_id):
+    return render_template(
+        "index.html",
+        lists=TodoList.query.all(),
+        todos=TodoItem.query.filter_by(list_id=list_id).order_by("id").all(),
+    )
+
+
 @app.route("/")
 def index():
-    return render_template("index.html", data=TodoItem.query.order_by("id").all())
+    return redirect(url_for("get_list_todos", list_id=1))
 
 
 @app.route("/todo/create", methods=["POST"])
